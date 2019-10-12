dc3b7d42325ce89afae41f286e8c97761a456b4f
SantiagoOrtiz0630 <san.or.gue@gmail.com>
2019-10-12 10:32:21 -0500

    Updates

---
+++ precache-manifest.5f3e5455c0c36cf041a741fc2ea746c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40dff0b6832b6234b98451ab7091920a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "736e59486dd82b2a3715",
+    "url": "/portfolio/static/css/main.d6121caa.chunk.css"
+  },
+  {
+    "revision": "18f03ddedad363657b79",
+    "url": "/portfolio/static/js/2.ccb47d59.chunk.js"
+  },
+  {
+    "revision": "736e59486dd82b2a3715",
+    "url": "/portfolio/static/js/main.a9ba359f.chunk.js"
+  },
+  {
+    "revision": "c5306228cc83f6f6271c",
+    "url": "/portfolio/static/js/runtime-main.7676bd13.js"
+  }
+]);
